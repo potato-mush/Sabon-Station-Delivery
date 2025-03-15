0e4ad61c5f0c490129a8c70145460f20db66a4b3
potato-mush <eron.dayto123@gmail.com>
2025-03-15 08:18:12 +0800

    Initial Commit

--- partials/_footer.php
+++ partials/_footer.php
@@ -1,3 +1,7 @@
 <div class="footer container-fluid bg-dark text-light">
     <p class="text-center py-2 mb-0">Copyright © <?php echo date("Y"); ?> Sabon Station. All right reserved.</p>
+    <div class="text-center py-2">
+        <p class="mb-1">Developed by:</p>
+        <p class="mb-2">EJ | Jen | Josh</p>
+    </div>
 </div>
